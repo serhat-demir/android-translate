02f2f1f4df771a6555bd554dee0c653cc85aa118
serhatdemirr <serhatdmr@protonmail.com>
2022-07-11 14:41:47 +0300

    fix check language list

--- app/src/main/java/com/serhat/translate/MainActivity.java
+++ app/src/main/java/com/serhat/translate/MainActivity.java
@@ -79,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         if (!status) {
             loadLanguagesToLocalDb();
             spEdit.putBoolean("status", true);
+            spEdit.commit();
         }
     }
 
