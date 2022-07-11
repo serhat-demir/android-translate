0eb011e8d567ddf1138dc64e5ef18f3f445ee230
serhatdemirr <serhatdmr@protonmail.com>
2022-07-11 13:41:09 +0300

    add view binding

--- app/src/main/java/com/serhat/translate/MainActivity.java
+++ app/src/main/java/com/serhat/translate/MainActivity.java
@@ -2,13 +2,29 @@ package com.serhat.translate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.serhat.translate.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        context = MainActivity.this;
+
+        binding.toolbar.setTitle(getString(R.string.app_name));
+        binding.toolbar.setTitle(getString(R.string.google_translate_api));
+        setSupportActionBar(binding.toolbar);
+
+        binding.btnTranslate.setOnClickListener(view -> {
+            
+        });
     }
 }
